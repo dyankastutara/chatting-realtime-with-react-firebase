3ed37c57d1695f06c1c373b18013e20a08f2cf8e
dyankastutara <dyankastutara19@gmail.com>
2018-05-12 14:08:54 +0700

    add config firebase

---
+++ src/config/firebase.js
@@ -0,0 +1,13 @@
+
+import * as firebase from 'firebase';
+
+const config = {
+  apiKey: 'apiKey',
+  authDomain: 'projectId.firebaseapp.com',
+  databaseURL: 'https://databaseName.firebaseio.com',
+  storageBucket: 'bucket.appspot.com'
+};
+
+firebase.initializeApp(config);
+
+export const database = firebase.database();
